0fdbc3d887070f0d1213ebb9c2ada5eb539aed91
Gunhot <lkh0107@sogang.ac.kr>
2023-09-10 21:39:44 +0900

    first_commit(#6)

---
+++ test_code/test_url.py
@@ -0,0 +1,4 @@
+
+for i in range(10):
+    url = f'https://finance.naver.com/research/company_list.naver?keyword=&brokerCode=&writeFromDate=&writeToDate=&searchType=itemCode&itemName=%C4%AB%C4%AB%BF%C0&itemCode=035720&x=40&y=33&page={i}'
+    print(url)
